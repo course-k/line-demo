74ef4da22bb1d7aec6cc5f0b3caf9afdac21ab46
course-k <course.k.ky@gmail.com>
2024-03-24 15:53:19 +0900

    modifiy

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-/*パッケージのインポート */
-// botがhttpリクエストを処理できるようにhttpsパッケージをインポート
+// モジュールのインポート
 const https = require("https");
-// expressパッケージをインポート、インスタンス化
 const express = require("express");
-const app = express();
 
-/* 環境変数の追加 */
-// ポート番号の指定
-const PORT = process.env.PORT || 3001;
-// Messaging APIを呼び出すためのトークン（別の場所で指定）
+// 環境変数の取得
+// ポート番号
+const PORT = process.env.PORT || 3000;
+// Messaging APIを呼び出すためのトークン
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-/* ミドルウェアの設定 */
-// リクエストオブジェクトをJSON、文字列、配列として扱うための関数
+// Expressアプリケーションオブジェクトの生成
+const app = express();
+
+// ミドルウェアの設定
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, }));
 
-/* ルーティングの設定 */
+// ルーティングの設定-ドメインのルート
 app.get("/", (_, res) => {
   res.sendStatus(200);
 });
 
+//ルーティングの設定-MessaginAPI
 app.post("/webhook", (req, _) => {
   if (req.body.events[0].type === "message") {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + TOKEN,
+    };
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
@@ -36,10 +40,6 @@ app.post("/webhook", (req, _) => {
         },
       ],
     });
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    };
     const webhookOptions = {
       hostname: "api.line.me",
       path: "/v2/bot/message/reply",
@@ -61,6 +61,7 @@ app.post("/webhook", (req, _) => {
   }
 });
 
+// リスナーの設定
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
