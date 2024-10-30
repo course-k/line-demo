41b8325e6482536d83bd182a02d41cd42d3c34b6
practice_user <practice@mail.co.jp>
2024-10-30 17:41:38 +0900

    Revert "for giita cutomize"
    This reverts commit c99efd079d3b040881336b3ddca1c0acdbff43c6.

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 3000;
 // Messaging APIを呼び出すためのトークン
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
+const HEADERS = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + TOKEN,
+};
+
+const HOSTNAME = "api.line.me";
+
 // Expressアプリケーションオブジェクトの生成
 const app = express();
 
@@ -24,11 +31,26 @@ app.get("/", (_, res) => {
 //ルーティングの設定-MessaginAPI
 app.post("/webhook", (req, res) => {
   res.send("HTTP POST request sent to the webhook URL!");
+  let messages = [];
   switch (req.body.events[0].type) {
+    case "message":
+      messages.push({ type: "text", text: "Hello, user", })
+      messages.push({ type: "text", text: "May I help you?", })
+      break;
     case "follow":
-      const userData = { userId: req.body.events[0].source.userId }
+      messages.push({ type: "text", text: "Nice to meet you!", });
+      const userData = {
+        userId: req.body.events[0].source.userId
+      }
       fs.writeFileSync('./user_data.json', JSON.stringify(userData));
   }
+  autoReply(req, messages);
+});
+
+app.get("/push", (req, res) => {
+  res.send("HTTP POST request sent to the webhook URL!");
+  const messages = [{ type: "text", text: "push message!", }];
+  pushMessage(messages);
 });
 
 // リスナーの設定
@@ -36,27 +58,41 @@ app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
 
-app.get("/push", (req, res) => {
-  res.send("HTTP POST request sent to the push URL!");
-  const messages = [{ type: "text", text: "push message!", }];
-  pushMessage(messages);
-});
+function autoReply(req, messages) {
+
+  const dataString = JSON.stringify({
+    replyToken: req.body.events[0].replyToken,
+    messages: messages,
+  });
+  const webhookOptions = {
+    hostname: HOSTNAME,
+    path: "/v2/bot/message/reply",
+    method: "POST",
+    headers: HEADERS,
+    body: dataString,
+  }
+  const request = https.request(webhookOptions, res => {
+    res.on("data", d => {
+      process.stdout.write(d);
+    });
+  });
+  request.on("error", err => {
+    console.error(err);
+  });
+
+  request.write(dataString);
+  request.end();
+}
 
 function pushMessage(messages) {
-  const HEADERS = {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + TOKEN,
-  };
-
   const userData = JSON.parse(fs.readFileSync('./user_data.json', 'utf-8'));
   const userId = userData.userId;
   const dataString = JSON.stringify({
     to: userId,
     messages: messages,
   });
-
   const webhookOptions = {
-    hostname: "api.line.me",
+    hostname: HOSTNAME,
     path: "/v2/bot/message/push",
     method: "POST",
     headers: HEADERS,
